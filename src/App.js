be8e46015e07107350ba82afefba3c45ebf42411
harshwardhanpjdhav <harshwardhanpj2001@gmail.com>
2022-01-17 14:23:48 +0530

    Added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import Post from './Post.js';
 function App() {
   return (
     <div className="App">
-      <h1>lets bulid insta colne</h1>
+      
+
+      <header className="app--header">
+        <img className="logoimage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt="" />
+        
+          <input className="header-search" type="search" name="" id="" />
+        
+      </header>
+
+      <Post/>
+
+
     </div>
   );
 }
